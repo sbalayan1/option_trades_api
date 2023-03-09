8c2d627c9bb22685f0ef36f6f8335039a3fbbe7d
sbalayan1 <balayans2014@yahoo.com>
2023-03-09 09:51:04 -0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,6 @@ require 'faker'
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
-# t.string :ticker, :null => false
-# t.date :buy_date, :null => false 
-# t.date :sell_date, :null => false
-# t.float :strike, :null => false
-# t.string :type, :null => false
-# t.integer :time_to_expiration, :null => false
-# t.integer :quantity, :null => false
-# t.float :buy_price, :null => false
-# t.float :sell_price, :null => false
-
 puts "Clearing database"
 OptionTrade.destroy_all()
 
@@ -26,16 +15,16 @@ puts 'Seeding database!!!!'
 
 # create an array of dates that are weekdays
 dates = []
-10.times do
+20.times do
     date = Faker::Date.between(from: 1.month.ago, to: Date.today) #generate a random date
     day_in_week = date.wday() #grab the dates day of the week
     dates.push(date) if day_in_week != 0 && day_in_week != 6 #add to the dates array if its not a Sunday or Saturday
 end
 
-#sort dates
-dates.sort
+#sort dates. dates need to be unique
+dates = dates.uniq.sort
 
-1.times do
+20.times do
     types = ["CALLS", "PUTS"]
     buy_date_idx = Random.new.rand(0..dates.length-1)
 
@@ -54,10 +43,7 @@ dates.sort
 
     exp_date_idx = Random.new.rand(sell_date_idx..dates.length-1)
     args[:expiration_date] = dates[exp_date_idx]
-
     OptionTrade.create(**args)
 end
-
-# OptionTrade.create(ticker: 'AAPL', buy_date: create_date(3, 7, 2023), sell_date: create_date(3, 8, 2023), strike: 150, option_type: 'PUTS', expiration_date: create_date(3, 10, 2023), quantity: 1, buy_price: 96, sell_price: 120)
 
 puts "Done seeding!"
